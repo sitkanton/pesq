be67b622b3a9c2cf9c08857fa023ccbb962a8308
sitkanton <149360808+sitkanton@users.noreply.github.com>
2024-12-27 17:08:00 +0300

    firstcommit

---
+++ src/assets/Logo.jsx
@@ -0,0 +1,14 @@
+export default function Logo() {
+  return (
+      <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="35" cy="35" r="35" fill="#9400D3"/>
+        
+        <defs>
+          <clipPath id="clip0_1_102">
+            <rect width="35.8333" height="16.6667" fill="white" transform="translate(17.5 55.8333)"/>
+          </clipPath>
+        </defs>
+      </svg>
+
+  );
+}
